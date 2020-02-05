d185c39a388b82062bdf7ffc197763e3096c8006
Federico Viceconti <Federico.Viceconti@nttdata.com>
2020-02-05 07:16:15 +0100

    edit config to mock

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -3,12 +3,13 @@ import 'package:flutter_slider/services/api.dart';
 /// The config class contains the principal configuration based, for example,
 /// on build type. You can here, every thing is related to configuration.
 class Config {
-  static Environment environment = Environment.DEV;
+  static Environment environment = Environment.MOCK;
 }
 
 class ClientPath {
   static const String SLIDES_PATH = "slides";
 }
+
 class KeyCode {
   static int left = 123;
   static int right = 124;
